1a7d22b4f517a5f45865101a42e513898a85dcad
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-19 14:03:52 +0200

    Added html of hero & media-text sections

---
+++ resources/views/partials/homepage/hero.blade.php
@@ -0,0 +1,11 @@
+<section class="hero">
+    <div class="container">
+      
+        <div class="banner">
+            <h3>Lezen</h3>
+            <h1><span>O</span>nline</h1>
+            <h4>Gratis e-Book Bibliotheek</h4>
+        </div>
+
+    </div>
+</section>
---
+++ resources/views/partials/homepage/media-text.blade.php
@@ -0,0 +1,30 @@
+<section class="media-text">
+    <div class="container">
+        <div class="left">
+            <div class="num">
+                <span>01</span>
+            </div>
+            <button class="main">
+                <a href="about.html">Ontdek</a>
+            </button>
+        </div>
+        <div class="right">
+            <h4>Over ons</h4>
+            <h1>Wie zijn wij?</h1>
+            <div class="border"></div>
+             <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+              pariatur. Commodi tempore, animi optio cumque omnis quaerat
+              adipisci reprehenderit! Soluta, velit officia. Doloribus repellat
+              fugiat dolor placeat eaque earum recusandae!
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+              quasi error doloribus pariatur officia temporibus. Mollitia
+              distinctio sit inventore deserunt. Delectus totam iusto voluptatem
+              fugit eveniet quisquam repudiandae sed quam dolor. Voluptatibus
+              atque exercitationem optio harum mollitia quaerat, id soluta.
+            </p>
+        </div>
+    </div>
+</section>
