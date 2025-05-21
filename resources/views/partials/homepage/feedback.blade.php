ed9da828ef511b5bab1d50441421ddbacbe5e942
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-21 12:08:45 +0200

    Added feedback section html and scss

---
+++ resources/views/partials/homepage/feedback.blade.php
@@ -0,0 +1,58 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<section class="feedback">
+    <div class="container">
+    <div class="top">
+        <div class="num">
+            <h1>03</h1>
+        </div>
+        <div class="header">
+            <h4>Klanten</h4>
+            <h1>Bevindingen</h1>
+            <div class="border"></div>
+        </div>
+    </div>
+    <div class="feedback">
+        <div class="top">
+            <div class="img-wrapper">
+            <img src="wp-content\themes\sage\resources\images\client-main.jpg" alt="">
+            </div>
+            <div class="heading">
+                <h1>Klanten</h1>
+                <h3>Feedback</h3>
+            </div>
+        </div>
+        <div class="bottom">
+           
+            <div class="quote">
+                 <div class="quote-bg"></div>
+                 <div class="content-wrapper">
+                <div class="header">
+                    <i class="fa-solid fa-quote-left"></i>
+                    <h1>Robert Smith</h1>
+                    <i class="fa-solid fa-quote-right"></i>
+                </div>
+                <div class="inner">
+                    <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+                    quidem laborum corrupti, in fugit rerum a dolor voluptatibus
+                    necessitatibus vel, ad ex quae autem dolorem quis eius quia
+                    aspernatur esse?
+                    </p>
+                </div>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button>
+        <a href="testimonials.html">Leer meer</a>
+    </button>
+    </div>
+</section>
