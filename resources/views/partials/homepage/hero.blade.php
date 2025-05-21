4cf37b974da2cc4954149dfa47a808080cdf3251
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-21 12:07:24 +0200

    Created hero section html and scss

--- resources/views/partials/homepage/hero.blade.php
+++ resources/views/partials/homepage/hero.blade.php
@@ -3,7 +3,7 @@
       
         <div class="banner">
             <h3>Lezen</h3>
-            <h1><span>O</span>nline</h1>
+            <h1>Online</h1>
             <h4>Gratis e-Book Bibliotheek</h4>
         </div>
 
