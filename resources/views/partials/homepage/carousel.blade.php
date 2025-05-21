c39277126bdb0edfc6c1d024675d9c764e267690
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-21 12:08:30 +0200

    Added carousel section html and scss

---
+++ resources/views/partials/homepage/carousel.blade.php
@@ -0,0 +1,37 @@
+<section class="carousel">
+    <div class="container">
+        <div class="content">
+                <div class="header">
+                    <h4>Boeken</h4>
+                    <h1>Onze Bestsellers</h1>
+                    <div class="border"></div>
+                </div>
+                <div class="num">
+                    <h1>02</h1>
+                </div>
+        </div>
+            <div class="list">
+                <div class="book">
+                    <img src="wp-content\themes\sage\resources\images\book1.png" alt="">
+                    <h2>De avonturen van Tom Sawyer</h2>
+                    <p>Geschreven door Mark Twain</p>
+                </div>
+
+                <div class="book">
+                    <img src="wp-content\themes\sage\resources\images\book2.png" alt="">
+                    <h2>De idioot</h2>
+                    <p>Geschreven door Fyodor Dostoyevsky</p>
+                </div>
+
+                <div class="book">
+                    <img src="wp-content\themes\sage\resources\images\book3.png" alt="">
+                    <h2>De graaf van Monte Cristo</h2>
+                    <p>Geschreven door Alexandre Dumas</p>
+                </div>
+            </div>
+            <button>
+                <a href="books.html">Bekijk alles</a>
+            </button>
+        
+    </div>
+</section>
