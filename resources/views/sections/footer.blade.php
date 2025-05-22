85c591736b822e8764d4c2b1aaa7dd91dc1099aa
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-22 10:37:48 +0200

    Created footer section included html and scss

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,53 @@
 <footer class="content-info">
   @php(dynamic_sidebar('sidebar-footer'))
+
+  <section class="footer">
+    <div class="container">
+      <div class="header">
+        <h4>Kom in contact</h4>
+        <h1>Neem contact met ons op</h1>
+        <div class="border"></div>
+      </div>
+      <div class="content">
+        <div class="social">
+          <div class="address">
+            <div class="top">
+                <i class="fa-regular fa-envelope"></i>
+                <p>Adresgegevens</p>
+            </div>
+            <div class="bottom">
+              <p>E-Book Bibliotheek</p>
+              <p>Teststraat 1</p>
+              <p>Amsterdam</p>
+              <p>Nederland</p>
+            </div>
+          </div>
+
+          <div class="social-media">
+              <i class="fa-brands fa-facebook-f"></i>
+              <i class="fa-brands fa-instagram"></i>
+              <i class="fa-brands fa-linkedin-in"></i>
+              <i class="fa-brands fa-x-twitter"></i>
+          </div>
+        </div>
+        <div class="form">
+          <form>
+              <div class="input-group">
+                  <input type="text" placeholder="Naam" />
+                  <input type="email" placeholder="Email" />
+                  <input type="text" placeholder="Telefoonnummer" />
+                  <input type="text" placeholder="Adres" />
+              </div>
+               <textarea placeholder="Bericht"></textarea>
+              <button type="button">Klaar</button>
+          </form>
+        </div>
+      </div>
+      <div class="bottom">
+            <p>&copy; Gratis E-book Bibliotheek. All Rights Reserved</p>
+            <button type="button">
+            <a href="#">Terug naar boven <i class="fa-solid fa-chevron-up"></i></a>
+      </div>
+    </div>
+  </section>
 </footer>
