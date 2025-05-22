a5ae4090116b61e6ba1b5aaf8e03247d519bf312
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-22 12:23:04 +0200

    Added features section to about page

---
+++ resources/views/partials/about/features.blade.php
@@ -0,0 +1,51 @@
+<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+
+<section class="features">
+    <div class="container">
+
+        <div class="header">
+            <h1>Onze mogelijkheden</h1>
+            <div class="border"></div>
+        </div>
+
+        <div class="feature">
+            <span class="material-symbols-outlined">import_contacts</span>
+            <h3>Brede selectie E-books</h3>
+        </div>
+
+        <div class="feature">
+            <span class="material-symbols-outlined"> grid_view </span>
+            <h3>Gebruiksvriendelijke Layout</h3>
+          </div>
+
+          <div class="feature">
+            <span class="material-symbols-outlined"> library_books </span>
+            <h3>Toegankelijke Formaten</h3>
+          </div>
+
+          <div class="feature">
+            <span class="material-symbols-outlined"> forum </span>
+            <h3>Betrokken Community</h3>
+          </div>
+
+          <div class="feature">
+            <span class="material-symbols-outlined"> school </span>
+            <h3>Educatieve Middelen</h3>
+          </div>
+
+          <div class="feature">
+            <span class="material-symbols-outlined"> language </span>
+            <h3>Verschillende Talen Beschikbaar</h3>
+          </div>
+
+          <div class="feature">
+            <span class="material-symbols-outlined"> wifi_off </span>
+            <h3>Offline Lezen</h3>
+          </div>
+
+          <div class="feature">
+            <span class="material-symbols-outlined"> signature </span>
+            <h3>Auteur Promoties</h3>
+          </div>
+    </div>
+</section>
