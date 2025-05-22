1ab9fc8a895424e36c9b950cce8422f32125cae3
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-22 12:22:44 +0200

    Imported about page

---
+++ resources/views/template-about.blade.php
@@ -0,0 +1,15 @@
+{{--
+  Template Name: About Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php(the_post())
+    {{-- @include('partials.page-header')
+    @include('partials.content-page') --}}
+    @include('partials.about.hero')
+    @include('partials.about.features')
+    @include('partials.about.team')
+  @endwhile
+@endsection
