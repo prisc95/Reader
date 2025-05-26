1737bbeed264e9656f19e49f41734e87255ba04f
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-05-26 10:18:45 +0200

    Changed hero section name to fix duplicate image issue

---
+++ resources/views/partials/about/about-hero.blade.php
@@ -0,0 +1,8 @@
+<link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+
+<section class="about-hero">
+    <div class="container">
+            <h4>Over ons</h4>
+            <h1>Ontdek het allemaal</h1>
+    </div>
+</section>
