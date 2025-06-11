ce325376c7a564641b07a703a89e7b5f192081f5
Priscilla de Groot <priscilla.degroot@hotmail.com>
2025-06-11 09:38:09 +0200

    Changed names on teams page

--- resources/views/partials/about/team.blade.php
+++ resources/views/partials/about/team.blade.php
@@ -6,19 +6,19 @@
         <div class="members">
             <div class="member">
                 <img src="{{ get_template_directory_uri() }}/resources/images/member1.jpg" data-pin-no-hover="true" alt="">
-                <h2>John Roberts</h1>
+                <h2>Mark Goederee</h1>
                 <h3>Digitale Archivaris</h3>
             </div>
 
             <div class="member">
                 <img src="{{ get_template_directory_uri() }}/resources/images/member2.jpg" alt="">
-                <h2>Ann Brown</h1>
+                <h2>Tess de Vries</h1>
                 <h3>Beheerder</h3>
             </div>
 
             <div class="member">
                 <img src="{{ get_template_directory_uri() }}/resources/images/member3.jpg" alt="">
-                <h2>Alice Winters</h1>
+                <h2>Alice Maarssen</h1>
                 <h3>Community Manager</h3>
             </div>
         </div>
